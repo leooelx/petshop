f8da5d8d4fe77420759bf33350ea6ef7dba3d193
Leonardo Viana Leite <leonardo.viana@ssys.com.br>
2021-10-31 16:44:31 -0300

    CRUD de Categoria (Post, Put, Delete) + Manipulação do Exception

--- src/main/java/com/leo/demo/service/CategoriaService.java
+++ src/main/java/com/leo/demo/service/CategoriaService.java
@@ -3,10 +3,12 @@ package com.leo.demo.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.leo.demo.domain.Categoria;
 import com.leo.demo.repository.CategoriaRepository;
+import com.leo.demo.service.exceptions.DataIntegrityException;
 import com.leo.demo.service.exceptions.ObjetoNaoEncontradoException;
 
 @Service
@@ -18,6 +20,26 @@ public class CategoriaService {
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow( () -> new ObjetoNaoEncontradoException( "Objeto não encontrado. ID: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}	
+	
+	public void delete(Integer id) {
+		find(id);		
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Categoria possui produtos, não é possível deletar!");
+		}
+		
 	}
 
 }
