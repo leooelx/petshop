350768245bd0145a157b2e9e55660f7498a535a0
Leonardo Viana Leite <leonardo.viana@ssys.com.br>
2021-10-31 12:01:00 -0300

    Adicionando classe de Modelo Categoria e Rest

---
+++ src/main/java/com/leo/demo/domain/Categorias.java
@@ -0,0 +1,60 @@
+package com.leo.demo.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Categorias implemets Serializable {
+	
+	private static final log seriaLVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	
+	public Categoria() {
+		
+	}
+
+	public Categorias(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+	
+	
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categorias other = (Categorias) obj;
+		return Objects.equals(id, other.id) && Objects.equals(nome, other.nome);
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+
+}
--- src/main/java/com/leo/demo/resources/CategoriaResourse.java
+++ src/main/java/com/leo/demo/resources/CategoriaResourse.java
@@ -1,16 +1,31 @@
 package com.leo.demo.resources;
 
+import java.util.List;
+
 import org.spingframework.web.bind.annotation.RequestMapping;
 import org.spingframework.web.bind.annotation.RequestMethod;
 import org.spingframework.web.bind.annotation.RestController;
+
+import com.leo.demo.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResourse {
 	
 	@RequestMapping(met = RequestMethod.GET)
-	public String mostrar() {
-		return "Teste REST Categorias";
+	public List<Categoria> mostrar() {
+		
+		Categoria cat = new Categoria(1, "Alimento");
+		Categoria cat = new Categoria(1, "Alimento");
+		Categoria cat = new Categoria(1, "Alimento");
+		
+		List<Categoria> list = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		lista.add(cat3);
+		
+		return lista;
 	}
 
 }
