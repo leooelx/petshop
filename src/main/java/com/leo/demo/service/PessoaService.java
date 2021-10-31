4a74d2556753bce8076fd9d97b64ba3f68bf62c1
Leonardo Viana Leite <leonardo.viana@ssys.com.br>
2021-10-31 16:01:25 -0300

    disponibilizar endpoint /pessoas/{id}

---
+++ src/main/java/com/leo/demo/service/PessoaService.java
@@ -0,0 +1,48 @@
+package com.leo.demo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import com.leo.demo.domain.Pessoa;
+import com.leo.demo.repository.PessoaRepository;
+import com.leo.demo.service.exceptions.ObjetoNaoEncontradoException;
+
+@Service
+public class PessoaService {
+	
+	@Autowired
+	private PessoaRepository repo;
+	
+	public Pessoa find(Integer id) {
+		Optional<Pessoa> obj = repo.findById(id);
+		return obj.orElseThrow( () -> new ObjetoNaoEncontradoException( "Objeto não encontrado. ID: " + id + ", Tipo: " + Pessoa.class.getName()));
+	}
+	
+	public Pessoa insert(Pessoa obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Pessoa update(Pessoa obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}	
+	
+	public void delete(Integer id) {
+		find(id);		
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Pessoa não pode ser deletada!");
+		}		
+	}
+
+	public List<Pessoa> findAll() {		
+		return repo.findAll();
+	}
+
+}
