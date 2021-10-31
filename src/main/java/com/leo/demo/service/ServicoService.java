dbb5c9a6ecdfea8b635af4131e5ee214a50a2a83
Leonardo Viana Leite <leonardo.viana@ssys.com.br>
2021-10-31 16:36:29 -0300

    Disponibilizado Servicos/{id} e corrigido referencias ciclicas

---
+++ src/main/java/com/leo/demo/service/ServicoService.java
@@ -0,0 +1,46 @@
+package com.leo.demo.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import com.leo.demo.domain.Servico;
+import com.leo.demo.domain.Servico;
+import com.leo.demo.repository.ServicoRepository;
+import com.leo.demo.service.exceptions.DataIntegrityException;
+import com.leo.demo.service.exceptions.ObjetoNaoEncontradoException;
+
+@Service
+public class ServicoService {
+	
+	@Autowired
+	private ServicoRepository repo;
+	
+	public Servico find(Integer id) {
+		Optional<Servico> obj = repo.findById(id);
+		return obj.orElseThrow( () -> new ObjetoNaoEncontradoException( "Objeto não encontrado. ID: " + id + ", Tipo: " + Servico.class.getName()));
+	}
+	
+	public Servico insert(Servico obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Servico update(Servico obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}	
+	
+	public void delete(Integer id) {
+		find(id);		
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Servico possui produtos, não é possível deletar!");
+		}
+		
+	}
+
+}
